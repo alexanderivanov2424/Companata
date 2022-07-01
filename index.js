3ab895f629e5329f7f612974095dd0b50f472d45
alexander_ivanov@brown.edu <alexander_ivanov@brown.edu>
2022-06-30 22:31:48 -0400

    added some functions for moving around items and money

--- index.js
+++ index.js
@@ -95,6 +95,7 @@ const state = {
   ],
   target: "", //person who bid the most or target in Targeting phase
   timer: 0,
+  confirms: [],
   players: [
     {
       name: "Joe",
@@ -211,6 +212,93 @@ function initGameState(lobby){
   });
 }
 
+function PotValue(playerName){
+  const pot = state.players[playerName].pot;
+  return pot.v5 * 5 + pot.v10 * 10 + pot.v25 * 25 + pot.v50 * 50;
+}
+
+function SendPot(fromName, toName){
+  const fromPot = state.players[fromName].pot;
+  state.players[toName].wallet.v0 += fromPot.v0;
+  state.players[toName].wallet.v5 += fromPot.v5;
+  state.players[toName].wallet.v10 += fromPot.v10;
+  state.players[toName].wallet.v25 += fromPot.v25;
+  state.players[toName].wallet.v50 += fromPot.v50;
+}
+
+function ClaimStand(playerName){
+  for(var i = 0; i < state.stand.length; i++){
+    state.players[playerName].items[state.stand[i]] = (state.players[playerName].items[state.stand[i]] || 0) + 1;
+  }
+  state.stand = [];
+}
+
+function HasItem(playerName, item){
+  if(!(item in state.players[playerName].items)){
+    return false;
+  }
+  return state.players[playerName].items[item] > 0;
+}
+
+function SubmitItemToStand(playerName, item){
+  if(!HasItem(playerName, item)){
+    console.error("Adding item to stand which player doesn't have");
+    return;
+  }
+  state.players[playerName].items[item] -= 1;
+  state.stand.push(item);
+}
+
+function HasCoin(playerName, coin){
+  return state.players[playerName].wallet[coin] > 0;
+}
+
+function PayCoin(fromPlayer, toPlayer, coin){
+  if(!HasCoin(fromPlayer, coin)){
+    console.error("Taking coin from player which player doesn't have");
+    return;
+  }
+  state.players[fromPlayer].wallet[coin] -= 1;
+  state.players[toPlayer].wallet[coin] += 1;
+}
+
+function playerMoney(playerName){
+  const wallet = state.players[playerName].wallet;
+  return wallet.v5 * 5 + wallet.v10 * 10 + wallet.v25 * 25 + wallet.v50 * 50;
+}
+
+function Pay(fromPlayer, toPlayer, amount){
+  if(playerMoney(fromPlayer) < amount){
+    console.error("Player paying more money than they have");
+    return;
+  }
+  const values = [50, 25, 10, 5];
+  const valueNames = [v50, v25, v10, v5];
+
+  for(var i = 0; i < values.length; i++){
+    while(amount >= values[i] && HasCoin(fromPlayer, valueNames[i])){
+      amount-=values[i];
+      PayCoin(fromPlayer, toPlayer, valueNames[i]);
+    }
+  }
+  if(amount < 0){
+    console.error("Alex you dumbass go fix the Pay code");
+    return;
+  }
+  if(amount > 0){
+    for(var i = values.length - 1; i >=0; i--){ 
+      if(HasCoin(fromPlayer, valueNames[i])){
+        amount -= values[i];
+        PayCoin(fromPlayer, toPlayer, valueNames[i]);
+        break; //paying smallest coin available should be enough
+      }
+    }
+    if(amount > 0){
+      console.error("Alex you dumbass go fix the Pay code, trying to be clever smh");
+    }
+  }
+}
+
 function StartBiddingPhase(){
   if(state.phase !== ACTION_SELECTION){
     console.warn("Attempt to start bidding while not in action select phase");
@@ -288,7 +376,16 @@ function Target(playerName, item){
 }
 
 function ConfirmBidVersus(playerName){ //who is confirming
-
+  if(state.confirms.contains(playerName)){
+    console.warn("confirming player already confirmed");
+    return;
+  }
+  state.confirms.push(playerName);
+  if(state.confirms.contains(ownerPlayer) && state.confirms.contains(targetPlayer)){
+    state.confirms = [];
+    
+    
+  }
 }
 
 function ChoosePass(){ //owner player passes on new item
@@ -308,7 +405,7 @@ function NewActionSelectPhase(){ //reset who retracted bids
 }
 
 function InfuseFunds(){
-  
+
 }
 
 // -------------- Socket.IO Events ----------------
